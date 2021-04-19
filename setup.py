0b7e37fbb59059343323afb001660468cc6f2b9d
GasinAn <Gasin185@163.com>
2021-04-20 01:35:51 +0800

    __setup.py _setup.py > setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,20 @@ from Cython.Build import cythonize
 from numpy import get_include
 
 setup(
+    name='vis_precision',
+    ext_modules=cythonize('vis_precision.pyx'),
+    include_dirs=[get_include()],
+    zip_safe=False,
+)
+
+setup(
+    name='dnb_int32',
+    ext_modules=cythonize('dnb_int32.pyx'),
+    include_dirs=[get_include()],
+    zip_safe=False,
+)
+
+setup(
     name='dnb_float32',
     ext_modules=cythonize('dnb_float32.pyx'),
     include_dirs=[get_include()],
