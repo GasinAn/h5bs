343cb725a34676545c51a174013bfb8cc3e93c82
GasinAn <Gasin185@163.com>
2021-04-21 02:31:12 +0800

    update C version of float32 bit round

--- float32_bit_round.c
+++ float32_bit_round.c
@@ -1,4 +1,5 @@
 // Round val to val_r = n*2**b (int n; int b = max(b: 2**b <= g_max)).
+// Warning: undefined behavior when delta_exponent >= 32.
 
 #include <stdio.h>
 
@@ -11,9 +12,20 @@ float float32_bit_round(float val, float g_max){
 
     int delta_exponent = (exponent_val - exponent_g_max) >> 23;
 
-    unsigned int val_ = *p_val + (0x00400000 >> delta_exponent);
-    unsigned int val_r = val_ & (-8388608 >> delta_exponent);
+    // Situation: delta_exponent >= 0,
+    // return trunc_to_mul_of_2_to_b(val + sgn(val) * 2 ** (b - 1)).
+    unsigned int val_r_dexp_ge_0;
+    val_r_dexp_ge_0 = *p_val + (0x00400000 >> delta_exponent);
+    val_r_dexp_ge_0 = val_r_dexp_ge_0 & (-8388608 >> delta_exponent);
+    val_r_dexp_ge_0 = (delta_exponent > -1) * val_r_dexp_ge_0;
 
+    // Situation: delta_exponent == -1,
+    // return sgn(val) * g_max.
+    unsigned int val_r_dexp_eq_m1;
+    val_r_dexp_eq_m1 = (*p_val & -2147483648) | exponent_g_max;
+    val_r_dexp_eq_m1 = (delta_exponent == -1) * val_r_dexp_eq_m1;
+
+    unsigned int val_r = val_r_dexp_ge_0 + val_r_dexp_eq_m1;
     return *((float*) &val_r);
 }
 
