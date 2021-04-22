e154e15d3d80da2892d4d58e01e8793af71cf684
GasinAn <Gasin185@163.com>
2021-04-22 17:34:03 +0800

    use macro to make float32_bit_round.c more readable.

--- float32_bit_round.c
+++ float32_bit_round.c
@@ -3,26 +3,31 @@
 
 #include <stdio.h>
 
+#define HEX_00400000 4194304
+#define HEX_7F800000 2139095040
+#define HEX_80000000 -2147483648
+#define HEX_FF800000 -8388608
+
 float float32_bit_round(float val, float g_max){
     unsigned int *p_val = (int*) &val;
     unsigned int *p_g_max = (int*) &g_max;
 
-    int exponent_val = *p_val & 0x7f800000;
-    int exponent_g_max = *p_g_max & 0x7f800000;
+    int exponent_val = *p_val & HEX_7F800000;
+    int exponent_g_max = *p_g_max & HEX_7F800000;
 
     int delta_exponent = (exponent_val - exponent_g_max) >> 23;
 
     // Situation: delta_exponent >= 0,
     // return trunc_to_mul_of_2_to_b(val + sgn(val) * 2 ** (b - 1)).
     unsigned int val_r_dexp_ge_0;
-    val_r_dexp_ge_0 = *p_val + (0x00400000 >> delta_exponent);
-    val_r_dexp_ge_0 = val_r_dexp_ge_0 & (-8388608 >> delta_exponent);
+    val_r_dexp_ge_0 = *p_val + (HEX_00400000 >> delta_exponent);
+    val_r_dexp_ge_0 = val_r_dexp_ge_0 & (HEX_FF800000 >> delta_exponent);
     val_r_dexp_ge_0 = (delta_exponent > -1) * val_r_dexp_ge_0;
 
     // Situation: delta_exponent == -1,
     // return sgn(val) * g_max.
     unsigned int val_r_dexp_eq_m1;
-    val_r_dexp_eq_m1 = (*p_val & -2147483648) | exponent_g_max;
+    val_r_dexp_eq_m1 = (*p_val & HEX_80000000) | exponent_g_max;
     val_r_dexp_eq_m1 = (delta_exponent == -1) * val_r_dexp_eq_m1;
 
     unsigned int val_r = val_r_dexp_ge_0 + val_r_dexp_eq_m1;
